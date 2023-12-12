4b3eeb96e547b6924693c51406d11472e3a5b2f9
Leonardo <leosimoes.engcomp@gmail.com>
2023-12-12 20:09:57 -0300

    ProfileEnum modified

--- src/main/java/com/udemy/helpdesk/api/enums/ProfileEnum.java
+++ src/main/java/com/udemy/helpdesk/api/enums/ProfileEnum.java
@@ -1,7 +1,27 @@
 package com.udemy.helpdesk.api.enums;
 
 public enum ProfileEnum {
-    ROLE_ADMIN,
-    ROLE_CUSTOMER,
-    ROLE_TECHNICIAN;
+    ROLE_ADMIN("Admin"),
+    ROLE_CUSTOMER("Customer"),
+    ROLE_TECHNICIAN("Technician");
+
+    private final String value;
+
+    ProfileEnum(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static ProfileEnum fromValue(String value) {
+        String valueCapitalized =  value.substring(0, 1).toUpperCase() + value.substring(1).toLowerCase();
+        for (ProfileEnum profile : ProfileEnum.values()) {
+            if (profile.value.equals(value)) {
+                return profile;
+            }
+        }
+        return null;
+    }
 }
