6ba9e5fa1af6ae6944aa7d462f7b937558cc72c9
Leonardo <leosimoes.engcomp@gmail.com>
2023-12-16 11:42:46 -0300

    JwtAuthenticationRequest Class modified

--- src/main/java/com/udemy/helpdesk/api/security/jwt/JwtAuthenticationRequest.java
+++ src/main/java/com/udemy/helpdesk/api/security/jwt/JwtAuthenticationRequest.java
@@ -1,9 +1,11 @@
 package com.udemy.helpdesk.api.security.jwt;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
 
+@Data
 public class JwtAuthenticationRequest implements Serializable {
 
     private static final long serialVersionUID = 1L;
